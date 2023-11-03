e9037edba39c025cd0ef895d0893036f9f2b30e0
Eternal <124017017+Kevanko@users.noreply.github.com>
2023-11-03 10:03:08 +0000

    Create default project structure

---
+++ src/bloomfilter/filter.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+#include <bloomfilter/filter.h>
---
+++ src/source/source.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include <bloomfilter/filter.h>
+
+int main(){
+  printf("Hello World\n");
+  return 0;
+}
